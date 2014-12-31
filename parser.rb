83ca2c7ce473dc77003107b7867b669cbd23755c
Arash Badie Modiri <arashbm@gmail.com>
2014-12-31 18:42:40 +0330

    adding map through head and tail (cdr, car)

--- parser.rb
+++ parser.rb
@@ -2,7 +2,22 @@ class UnknownFunctionError < StandardError; end
 class ArityError < StandardError; end
 class NotAListError < StandardError; end
 
-def parse_commands(commands, c = {})
+def default_context
+  {
+    prepend: [:lambda,
+              [:list, :h, :t],
+              [:join, [:list, :h], :t]],
+    map: [:lambda,
+          [:list, :l, :x],
+          [:prepend,
+           [:x, [:head, :l]],
+           [:map, [:tail, :l], :x]]],
+    # prepend: [:lambda, [:list, :h, :t], [:join, [:list, :h], :t]],
+    # prepend: [:lambda, [:list, :h, :t], [:join, [:list, :h], :t]],
+  }
+end
+
+def parse_commands(commands, c = default_context)
   if commands.count == 1  # last command, we need the results (not the context)
     tparse(commands[0], c)
   else
@@ -18,7 +33,7 @@ end
 
 def parse(list, c)
   c = c.dup
-  # puts "parsing: #{list}"
+  puts "parsing: #{list}" if ENV["VERBOSE"]
   if list.kind_of? Array
     list[0] = tparse(list[0], c)
     case list[0]
@@ -54,16 +69,37 @@ def parse(list, c)
       [(tparse(list[1], c) ? tparse(list[2], c) : tparse(list[3], c)), c]
     when :list
       require_arity((1..Float::INFINITY), list)
-      [list, c]
+      [[:list, *(list[1..-1].map{ |a| tparse(a, c)  })], c]
     when :upto
       require_arity(2, list)
       [[:list, *(tparse(list[1], c)..tparse(list[2], c)).to_a], c]
-    when :map
+    when :head, :car
+      require_arity(1, list)
+      [list_items(list[1], c)[0], c]
+    when :tail, :cdr
+      require_arity(1, list)
+      v = list_items(list[1], c)
+      [[:list, *v[1..-1]], c]
+    # when :map
+    #   require_arity(2, list)
+    #   [[:list, *list_items(list[1], c).map { |i| tparse([list[2], i], c) }], c]
+    when :reduce
       require_arity(2, list)
-      [[:list, *list_items(list[1], c).map { |i| tparse([list[2], i], c) }], c]
+      [tparse(list_items(list[1], c).reduce { |m, x| [tparse(list[2], c), m, x]}, c), c]
     when :select
       require_arity(2, list)
       [[:list, *list_items(list[1], c).select { |i| tparse([list[2], i], c) }], c]
+    when :list?
+      require_arity(1, list)
+      begin
+        list_items(list[1], c)
+        [true, c]
+      rescue NotAListError
+        [false, c]
+      end
+    when :join
+      require_arity(2, list)
+      [[:list, *(list_items(list[1], c) + list_items(list[2], c))], c]
     when :each
       require_arity(2, list)
       items = list_items(list[1], c).each { |i| tparse([list[2], i], c) }
@@ -71,7 +107,7 @@ def parse(list, c)
     when :p
       require_arity(1, list)
       r = tparse(list[1], c)
-      puts r
+      p r
       [r, c]
     when :lambda # function def
       require_arity(2, list)
